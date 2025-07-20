bfa5fc70da98d9dc8b05439c48f2aded1d1ed89e
Sudhil Gauchan <sudhilgauchan@MacBook-Pro.attlocal.net>
2025-07-19 23:50:03 -0500

    Added Validator

---
+++ src/main/java/com/springbootjpa/exception/ObjectValidationException.java
@@ -0,0 +1,16 @@
+package com.springbootjpa.exception;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Getter
+public class ObjectValidationException extends RuntimeException{
+
+    private final Set<String> violations;
+
+    private final String violationSource;
+}
---
+++ src/main/java/com/springbootjpa/validator/ObjectsValidator.java
@@ -0,0 +1,28 @@
+package com.springbootjpa.validator;
+
+import com.springbootjpa.exception.ObjectValidationException;
+import org.springframework.stereotype.Component;
+
+
+import javax.validation.*;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class ObjectsValidator<T> {
+
+    private final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = validatorFactory.getValidator();
+
+    public void validate(T objectToValidate){
+        Set<ConstraintViolation<T>> violations = validator.validate(objectToValidate);
+        if(!violations.isEmpty()){
+            Set<String> errorMsg = violations
+                    .stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.toSet());
+            throw new ObjectValidationException(errorMsg, objectToValidate.getClass().getSimpleName());
+        }
+    }
+
+}
